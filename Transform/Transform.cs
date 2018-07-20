4a88f319a198ca57c6595ae3c815532eba497856
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-07-20 11:44:15 +0530

    Modified: Passing the function instead of using delegates

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -6,9 +6,9 @@ namespace Transform
 {
     public static class Transform 
     {
-        public delegate int del(int num);
+        //public delegate int del(int num);
 
-        public static int[] Map(this int[] numbers, del operation)
+        public static int[] Map(this int[] numbers, Func<int,int>operation)
         {
             int[] result = new int[numbers.Length];
             for (int i = 0; i < numbers.Length; i++)
